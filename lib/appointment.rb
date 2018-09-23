8d88df595da25c9ae9acec90f8ce3e820ae485c5
kvdoherty <kvdoherty@gmail.com>
2018-09-23 21:19:16 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,7 +2,8 @@ class Appointment
 
 attr_accessor :date, :patient
   
-def initialize(date, patient)
+def initialize(doctor, date, patient)
+  @doctor  = doctor
   @date = date 
   @patient = patient 
 end 
